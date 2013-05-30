a109343e2222c4d25b65678d8b940031a0134213
Hari Phaneendra <hdattada@gmail.com>
2013-05-30 14:44:34 -0400

    Initial Commit

---
+++ CS795HW1_Test/DsecApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CS795HW1_Test
+{
+    public class DsecApp
+    {
+
+        public bool Check_Username(String uname)
+        {
+            bool special_character_notpresent = Checkfor_Special_Characters(uname);
+            if (uname.Length == 0)
+                return false;
+            else if (special_character_notpresent)
+                return true;
+            else
+                return false;
+
+        }
+
+        private static bool Checkfor_Special_Characters(String uname)
+        {
+            Regex objAlphaPattern = new Regex("^[a-zA-Z0-9 ]*$"); ;
+            bool special_character_notpresent = objAlphaPattern.IsMatch(uname);
+            return special_character_notpresent;
+        }
+
+
+        internal bool Check_Key(string p)
+        {
+            return false;
+        }
+    }
+ 
+
+    
+}
---
+++ CS795HW1_Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS795HW1_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public DsecApp Create_DSecApp_Instance()
+        {
+            DsecApp dapp = new DsecApp();
+            return dapp;
+        }
+        [TestMethod]
+        public void Returns_True_For_Login_Hari()
+        {
+            Assert.IsTrue(Create_DSecApp_Instance().Check_Username("Hari"));
+        }
+
+        [TestMethod]
+        public void Returns_False_For_EmptyString()
+        {
+            Assert.IsFalse(Create_DSecApp_Instance().Check_Username(""));
+        }
+
+        [TestMethod]
+        public void Returns_False_For_InvalidCharactersInString()
+        {
+            Assert.IsFalse(Create_DSecApp_Instance().Check_Username("@#$"));
+        }
+
+        [TestMethod]
+        public void Returns_True_For_Key12()
+        {
+            Assert.IsTrue(Create_DSecApp_Instance().Check_Key("12"));
+        }
+
+    }
+}
